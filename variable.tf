0a4d426f2f26aa5869aaa756c6ca7af42cab3307
idreeskhan1991 <92720694+idreeskhan1991@users.noreply.github.com>
2021-11-11 19:53:58 +0530

    Updated Key

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-  default = "AKIAWE6YBVP3AAZTNW47"
+  default = "AKIAWE6YBVP3KQZYMZWF"
   }
 variable "secret_key" {
-  default = "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"
+  default = "lrjSyD1Beg9E6c4G+DY7+OSSESNjGinfVPdG8RCm"
   }
 variable "region" {
   default = "ap-south-1"
